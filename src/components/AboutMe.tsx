7fade4af49a516169e71a19327cbefa01dbada96
PhatVN <vonguyenphat012@gmail.com>
2024-05-03 21:49:26 +0700

    first commit

---
+++ src/components/AboutMe.tsx
@@ -0,0 +1,61 @@
+"use client"
+import React, { useState } from 'react'
+import Image from 'next/image'
+const TAB_DATA = [
+   {
+      title: "Skills",
+      id: "skills",
+      content: (
+         <ul className="list-disc pl-2">
+            <li>Node.js</li>
+            <li>Express</li>
+            <li>JavaScript</li>
+            <li>React</li>
+            <li>Java</li>
+            <li>Spring boot</li>
+         </ul>
+      ),
+   },
+   {
+      title: "Education",
+      id: "education",
+      content: (
+         <ul className="list-disc pl-2">
+            <li>Duy Tan University</li>
+         </ul>
+      ),
+   },
+   {
+      title: "Certifications",
+      id: "certifications",
+      content: (
+         <ul className="list-disc pl-2">
+            <li>Empty</li>
+         </ul>
+      ),
+   },
+];
+
+export default function AboutMe() {
+
+   return (
+      <>
+         <div className='grid grid-cols-1 sm:grid-cols-2 sm:mt-[40px] mt-[20px]'>
+            <div className='flex items-center justify-center'>
+               <Image
+                  src="/images/dev.webp"
+                  alt="dev image"
+                  width={500}
+                  height={500}
+               />
+            </div>
+            <div className='ps-10 pt-[10px] sm:pt-0'>
+               <h1 className='text-white mb-4 text-3xl sm:text-4xl lg:text-4xl lg:leading-normal font-extrabold text-center sm:text-left'>About Me</h1>
+               <p className='text-white text-base sm:text-lg mb-6 lg:text-xl '>I am a full stack web developer with a passion for creating interactive and responsive web applications. I have experience working with JavaScript, React, Redux, Node.js, Express, HTML, CSS, Java, Spring boot, and Git. I am a quick learner and I am always looking to expand my knowledge and skill set. I am a team player and I am excited to work with others to create amazing applications.
+               </p>
+               
+            </div>
+         </div>
+      </>
+   )
+}
---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,62 @@
+'use client'
+import React from 'react'
+import Image from 'next/image'
+import Link from "next/link";
+import { TypeAnimation } from 'react-type-animation';
+
+export default function HeroSection() {
+   return (
+      <section>
+         <div className='sm:grid grid-cols-1 sm:grid-cols-2 '>
+            <div className='flex flex-col items-center sm:block'>
+               <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold text-center sm:text-left">
+                  <span className="bg-gradient-to-r from-green-400 to-[#a855f7] bg-clip-text text-transparent ">
+                     Hello, I&apos;m{" "}
+                  </span>
+                  <br />
+                  <TypeAnimation
+                     sequence={[
+                        'Pal03',
+                        1000,
+                        'Web Developer',
+                        1000,
+                        'Mobile Developer',
+                        1000
+                     ]}
+                     wrapper="span"
+                     speed={50}
+                     style={{ fontSize: '0.7em', display: 'inline-block' }}
+                     repeat={Infinity}
+                  />
+               </h1>
+               <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl text-center sm:text-left">
+                  Hi
+               </p>
+               <div>
+                  <Link
+                     href="/#contact"
+                     className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-r from-green-400 to-[#a855f7] hover:bg-slate-200 text-white text-center sm:text-left">
+                     Hire Me
+                  </Link>
+                  <Link
+                     href="/"
+                     className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-green-400 to-[#a855f7] hover:bg-slate-800 text-white mt-3 text-center sm:text-left">
+                     <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                        Download CV
+                     </span>
+                  </Link>
+               </div>
+            </div>
+            <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative mx-auto">
+               <Image
+                  src="/images/avt-ic.png"
+                  alt="hero image"
+                  className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 object-cover"
+                  width={300}
+                  height={300}
+               />
+            </div>
+         </div>
+      </section>
+   )
+}
