3e1521914051e85754cffd3176fde57666ab848f
PhatVN <vonguyenphat012@gmail.com>
2024-05-04 14:43:25 +0700

    update

---
+++ src/components/ProjectCarousel.tsx
@@ -0,0 +1,61 @@
+"use client"
+import React, { useState, useEffect } from 'react'
+import { Card, CardContent } from "@/components/ui/card"
+import {
+   Carousel,
+   CarouselContent,
+   CarouselItem,
+   CarouselNext,
+   CarouselPrevious,
+   type CarouselApi,
+} from "@/components/ui/carousel"
+
+
+export default function ProjectCarousel() {
+   const [api, setApi] = useState<CarouselApi>()
+   const [current, setCurrent] = useState(0)
+   const [count, setCount] = useState(0)
+   useEffect(() => {
+      if (!api) {
+         return
+      }
+      setCount(api.scrollSnapList().length)
+      setCurrent(api.selectedScrollSnap() + 1)
+
+      api.on("select", () => {
+         setCurrent(api.selectedScrollSnap() + 1)
+      })
+   }, [api])
+
+
+   return (
+      <div>
+         <Carousel
+            setApi={setApi}
+            opts={{
+               align: "center",
+            }}
+            className="w-full max-w-sm"
+         >
+            <CarouselContent>
+               {Array.from({ length: 5 }).map((_, index) => (
+                  <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3 xl:basis-1/4">
+                     <div className="">
+                        <Card className='border-none bg-[#181818] '>
+                           <CardContent className="flex aspect-square items-center justify-center">
+                              <span className="text-3xl font-semibold text-white">{index + 1}</span>
+                           </CardContent>
+                        </Card>
+                     </div>
+                  </CarouselItem>
+               ))}
+            </CarouselContent >
+            <CarouselPrevious className='invisible sm:visible text-white bg-[#8200fc] border-none' />
+            <CarouselNext className='invisible sm:visible text-white bg-[#8200fc] border-none'/>
+         </Carousel>
+         <div className="py-2 text-center text-sm text-muted-foreground">
+            Slide {current} of {count}
+         </div>
+      </div>
+   )
+}
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import ProjectCarousel from './ProjectCarousel'
 
 export default function Projects() {
    return (
       <>
-         <div className='mt-[10px] sm:mt-[30px]'>
+         <div className='mt-[10px] sm:mt-[30px] flex flex-col items-center'>
             <h1 className='text-white mb-4 text-3xl sm:text-4xl lg:text-4xl lg:leading-normal font-extrabold text-center'>
                <span className='bg-gradient-to-r from-[#8200fc] to-white bg-clip-text text-transparent'>Projects</span>
             </h1>
+            <ProjectCarousel/>
          </div>
+      
       </>
    )
 }
