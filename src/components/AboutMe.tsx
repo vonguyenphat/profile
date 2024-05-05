dbd7d025eb7b55129bca7b3485bbade89a8e18d5
PhatVN <vonguyenphat012@gmail.com>
2024-05-05 17:26:53 +0700

    update about me

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -17,7 +17,7 @@ export default function AboutMe() {
             </div>
             <div className='sm:ps-10 pt-[10px] sm:pt-0'>
                <h1 className='text-white mb-4 text-3xl sm:text-4xl lg:text-4xl lg:leading-normal font-extrabold text-center sm:text-left '><span className='bg-gradient-to-r from-[#8200fc] to-white bg-clip-text text-transparent'>About Me</span> </h1>
-               <p className='text-white text-base sm:text-lg mb-6 lg:text-xl text-justify tracking-[1px]'>I am a full stack web developer with a passion for creating interactive and responsive web applications. I have experience working with JavaScript, React, Redux, Node.js, Express, HTML, CSS, Java, Spring boot, and Git. I am a quick learner and I am always looking to expand my knowledge and skill set. I am a team player and I am excited to work with others to create amazing applications.
+               <p className='text-[#ffffff] mt-[20px] text-[15px] sm:text-lg mb-6 lg:text-[15px] text-left'>I am a full stack web developer with a passion for creating interactive and responsive web applications. I have experience working with JavaScript, React, Redux, Node.js, Express, HTML, CSS, Java, Spring boot, and Git. I am a quick learner and I am always looking to expand my knowledge and skill set. I am a team player and I am excited to work with others to create amazing applications.
                </p>
             </div>
          </div>
