08bbb2b9ff2c2a6afde97c9cc9ed371e1e955985
PhatVN <vonguyenphat012@gmail.com>
2024-05-05 20:50:24 +0700

    update

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -51,9 +51,9 @@ export default function HeroSection() {
             </div>
             <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative mx-auto">
                <Image
-                  src="/images/avt-ic.png"
+                  src="/images/avt_pal03.jpg"
                   alt="hero image"
-                  className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 object-cover"
+                  className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 object-cover rounded-full h-[230px] w-[230px] sm:h-[250px] sm:w-[250px] lg:h-[360px] lg:w-[360px]"
                   width={300}
                   height={300}
                />
